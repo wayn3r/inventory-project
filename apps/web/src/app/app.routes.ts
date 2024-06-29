388d8afb8677744dbf83587368efc35e71d5b68d
wayn3r <w.wayn3r@gmail.com>
2024-06-29 15:41:48 -0400

    feat: add home page

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [{
-  path: 'transacciones',
-  loadChildren: () => import('./modules/transaccion/transaccion.module').then(m => m.TransaccionModule)
-}];
+export const routes: Routes = [
+  {
+    path: 'transacciones',
+    loadChildren: () => import('./modules/transaccion/transaccion.module').then(m => m.TransaccionModule)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./common/components').then(m => m.HomeComponent)
+  },
+
+];
