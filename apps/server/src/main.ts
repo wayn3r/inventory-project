010589a06b50a492de65787cc3aafe80e30ebe8c
wayn3r <w.wayn3r@gmail.com>
2024-08-05 22:41:08 -0400

    feat: add static middleware

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  app.use(express.static('public/web/browser'));
+  app.setGlobalPrefix('/api');
+  app.use((req: express.Request, res: express.Response, next) => {
+    if (req.url.startsWith('/api')) {
+      return next();
+    }
+    res.sendFile('index.html', { root: 'public/web/browser' });
+  });
+  await app.listen(process.env.PORT || 3000);
+
+  const logger = new Logger('NestApplication');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
